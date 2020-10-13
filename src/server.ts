d69821ad86ce11800d26a355b9c17410f6cd8f7d
Fabio Almeida <fabiorecife@gmail.com>
2020-10-13 12:46:10 -0300

    Step 2 - Rotas parametros metodos HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express, { response } from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/users', (req, res) => {
+app.post('/users/:id', (req, res) => {
+    console.log(req.query)
+    console.log(req.params)
+    console.log(req.body)
+
     return res.json({message: 'Hello World'})
 })
 
