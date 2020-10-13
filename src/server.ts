dc7cee4ef1eb6c24c6a12d7e98f95f96335e8fed
Fabio Almeida <fabiorecife@gmail.com>
2020-10-13 18:15:48 -0300

    Step 5 - Criando orfanato sem imagem

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,36 @@
 import express, { response } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/Orphanage'
 
 import './database/connection'
 
 const app = express()
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = req.body
+
+    const repository = getRepository(Orphanage)
+    const orphanage = repository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+    
+    await repository.save(orphanage)
+
     return res.json({message: 'Hello World'})
 })
 
